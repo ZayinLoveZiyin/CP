1838ff5a010e57fcd4b3dfd6ea030b88dbde8954
ZayinLoveZiyin <18319392399@163.com>
2023-07-03 23:02:32 +0800

    add directional_tarjan.h

---
+++ algo/graph_theory/tarjan/directional_tarjan.h
@@ -0,0 +1,45 @@
+struct directional_tarjan {
+  directional_tarjan(int n_) : n(n_), dfn(n_), low(n_), G(n_) {}
+
+  void add_edge(int u, int v) { G[u].push_back(v); }
+
+  std::vector<int> get_scc() {
+    dfn.assign(n, -1);
+    low.assign(n, -1);
+    int timestamp = 0, bel_id = 0;
+    std::stack<int> stk;
+    std::vector<int> bel(n, -1);
+    std::function<void(int)> dfs = [&](int u) {
+      dfn[u] = low[u] = timestamp++;
+      stk.push(u);
+
+      for (int v : G[u]) {
+        if (!~dfn[v]) {
+          dfs(v);
+          low[u] = std::min(low[u], low[v]);
+        } else if (!~bel[v]) {
+          low[u] = std::min(low[u], dfn[v]);
+        }
+      }
+
+      if (dfn[u] == low[u]) {
+        while (1) {
+          int v = stk.top();
+          bel[v] = bel_id;
+          stk.pop();
+          if (u == v) break;
+        }
+        ++bel_id;
+      }
+    };
+    for (int i = 0; i < n; ++i)
+      if (!~dfn[i]) {
+        dfs(i);
+      }
+    return bel;
+  }
+
+  int n;
+  std::vector<int> dfn, low;
+  std::vector<std::vector<int>> G;
+};
