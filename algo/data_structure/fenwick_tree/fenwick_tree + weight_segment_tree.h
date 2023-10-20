6045321897f2f7fcd56904411b4e40661d82bc00
ZayinLoveZiyin <18319392399@163.com>
2023-10-20 21:05:59 +0800

    update fenwick_tree+weight_segment_tree

--- algo/data_structure/fenwick_tree/fenwick_tree + weight_segment_tree.h
+++ algo/data_structure/fenwick_tree/fenwick_tree + weight_segment_tree.h
@@ -3,53 +3,43 @@ class weight_segment_tree {
  public:
   weight_segment_tree(int n_) : rt(-1) { n = n_; }
 
-  void add(int& k, int l, int r, int a, int b, T d) {
+  int add(int k, int l, int r, int p, T d) {
+    if (p < l || p > r) return k;
     if (!~k) k = newnode();
-    if (a > b || l > b || a > r) return;
-    if (a <= l && r <= b) {
-      tag[k] += d;
-      return;
-    }
+    ++sum[k];
+    // dbgf("a", k, l, r, sum[k]);
+    if (l == r) return k;
     int mid = (l + r) >> 1;
-    add(lson[k], l, mid, a, b, d);
-    add(rson[k], mid + 1, r, a, b, d);
-    sum[k] = tags[k] * (r - l + 1);
-    if (~lson[k]) sum[k] += sum[lson[k]];
-    if (~rson[k]) sum[k] += sum[rson[k]];
+    lson[k] = add(lson[k], l, mid, p, d);
+    rson[k] = add(rson[k], mid + 1, r, p, d);
+    return k;
   }
 
-  inline void add(int l, int r, T d) {
-    assert(0 <= l && l < n);
-    assert(0 <= r && r < n);
-    add(rt, 0, n - 1, l, r, d);
+  inline void add(int p, T d) {
+    assert(0 <= p && p < n);
+    rt = add(rt, 0, n - 1, p, d);
   }
 
   T query(int k, int l, int r, int a, int b) {
-    assert(l <= a && b <= r);
-    if (a == l && r == b) return sum[k];
+    if (!~k || a > b || l > b || a > r) return 0;
+    if (a <= l && r <= b) {
+      // dbgf("q", k, l, r, a, b, sum[k]);
+      return sum[k];
+    }
     int mid = (l + r) >> 1;
-    T res = tag[k] * (b - a + 1);
-    if (b <= mid)
-      res += query(k << 1, l, mid, a, b);
-    else if (a > mid)
-      res += query(k << 1 | 1, mid + 1, r, a, b);
-    else
-      res += query(k << 1, l, mid, a, mid) +
-             query(k << 1 | 1, mid + 1, r, mid + 1, b);
-    return res;
+    return query(lson[k], l, mid, a, b) + query(rson[k], mid + 1, r, a, b);
   }
 
   inline T query(int l, int r) {
     if (l > r) return T();
     assert(0 <= l && l < n);
     assert(0 <= r && r < n);
-    return query(1, 0, n - 1, l, r);
+    return query(rt, 0, n - 1, l, r);
   }
 
  private:
   int newnode() {
     int k = sum.size();
-    tag.push_back(T());
     sum.push_back(T());
     lson.push_back(-1);
     rson.push_back(-1);
@@ -58,30 +48,42 @@ class weight_segment_tree {
 
   int n, rt;
   std::vector<int> lson, rson;
-  std::vector<T> tag, sum;
+  std::vector<T> sum;
 };
 
 // 0-index based
 template <typename T>
 class fenwick_tree_2d {
  public:
-  fenwick_tree_2d(int n_, int m_) : n(n_), m(m_) tree(n) {}
+  fenwick_tree_2d(int n_, int m_)
+      : n(n_), tree(n_, weight_segment_tree<T>(m_)) {}
 
-  void add(int x, T v) {
+  void add(int x, int y) {
     for (int i = x + 1; i <= n; i += i & -i) {
-      tree[i - 1] += v;
+      dbgf("add", i, y);
+      tree[i - 1].add(y, 1);
     }
   }
 
-  T prefix_sum(int x) {
+  T prefix_sum(int x, int ly, int ry) {
     auto ans = T();
-    for (int i = x + 1; i > 0; i -= i & -i) ans += tree[i - 1];
+    for (int i = x + 1; i > 0; i -= i & -i) {
+      T t = tree[i - 1].query(ly, ry);
+      dbgf("qeury", i, ly, ry, t);
+      ans += t;
+    }
     return ans;
   }
 
-  T range_sum(int l, int r) { return prefix_sum(r) - prefix_sum(l - 1); }
+  T range_sum(int lx, int rx, int ly, int ry) {
+    T tr = prefix_sum(rx, ly, ry);
+    dbg(tr);
+    T tl = prefix_sum(lx - 1, ly, ry);
+    dbg(tl);
+    return tr - tl;
+  }
 
  private:
   int n, m;
-  std::vector<T> tree;
+  std::vector<weight_segment_tree<T>> tree;
 };
